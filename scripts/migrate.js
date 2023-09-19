631f56bb6589025358e53826b344a88b180adec6
Cha <bocquel.cha@gmail.com>
2023-09-19 11:53:35 +0200

    essayage de migration, big rip

--- scripts/migrate.js
+++ scripts/migrate.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
 const mongoose = require('mongoose');
-const Thing = require('../models/surfDestination');
+const Airtable = require('../models/surfDestination');
+
+const result = require('dotenv').config();
+if (result.error) {
+  throw result.error;
+}
+console.log(result.parsed);
+
+
 
 mongoose.connect("mongodb+srv://SurfTeam:123456azerty@surfingaway.lg3w7hp.mongodb.net/surfspots", {
   useNewUrlParser: true,
@@ -9,58 +17,57 @@ mongoose.connect("mongodb+srv://SurfTeam:123456azerty@surfingaway.lg3w7hp.mongod
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
 const getAllSpots = async () => {
-    const cle = process.env.API_KEY; 
-    try {
-      const response = await axios.get(
-        "mongodb+srv://<SurfTeam>:<123456azerty>@surfingaway.lg3w7hp.mongodb.net/surfspots",
-        {
-          headers: {
-            Authorization: `Bearer ${cle}`,
-          },
-        }
-      );
-      console.log("all surf spots datas fetch successfully");
-      return response.data.records;
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  };
+  const cle = process.env.API_KEY; 
+  console.log(cle);
+  try {
+    const response = await axios.get(
+      "https://api.airtable.com/v0/appqndsWaTAmFBUIM/Surf%20Destinations?maxRecords=10&view=By%20Surf%20Break",
+      {
+        headers: {
+          Authorization: `Bearer ${cle}`,
+        },
+      }
+    );
+    console.log("all surf spots datas fetch successfully");
+    console.log(response.data)
+    return response.data.records;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-  
-  const fetchDataSurfSpots = async () => {
-    const records = await getAllSpots();
-    records.forEach(record => {
-      const thing = new Thing({
-        'Destination': req.body.Destination,
-        'Destination State/Country': req.body['Destination State/Country'],
-        'Photos': req.body.Photos,
-        'Surfline Link': req.body['Surfline Link'],
-        'Difficulty Level': req.body['Difficulty Level'],
-        'Surf Break': req.body['Surf Break'],
-        'Adress': req.body.Adress,
-        'Geocode': req.body.Geocode,
-        'Influencers': req.body.Influencers,
-        'Peak Surf Season Begins': req.body['Peak Surf Season Begins'],
-        'Peak Surf Season Ends': req.body['Peak Surf Season Ends'],
-        'Travellers': req.body.Travellers,
-        'City': req.body.City,
-        'Latitude': req.body.Latitude,
-        'Longitude': req.body.Longitude,
-      });
-      thing.save().then(
-        () => {
-          console.log('Record saved successfully!');
-        }
-      ).catch(
-        (error) => {
-          console.error(error);
-        }
-      );
+const fetchDataSurfSpots = async () => {
+  const records = await getAllSpots();
+  records.forEach(record => {
+    const airtable = new Airtable({
+      'Destination': record.fields.Destination,
+      'Destination State/Country': record.fields['Destination State/Country'],
+      'Photos': record.fields.Photos,
+      'Surfline Link': record.fields['Surfline Link'],
+      'Difficulty Level': record.fields['Difficulty Level'],
+      'Surf Break': record.fields['Surf Break'],
+      'Adress': record.fields.Adress,
+      'Geocode': record.fields.Geocode,
+      'Influencers': record.fields.Influencers,
+      'Peak Surf Season Begins': record.fields['Peak Surf Season Begins'],
+      'Peak Surf Season Ends': record.fields['Peak Surf Season Ends'],
+      'Travellers': record.fields.Travellers,
+      'City': record.fields.City,
+      'Latitude': record.fields.Latitude,
+      'Longitude': record.fields.Longitude,
     });
-  };
+    airtable.save().then(
+      () => {
+        console.log('Record saved successfully!');
+      }
+    ).catch(
+      (error) => {
+        console.error(error);
+      }
+    );
+  });
+};
 
-  
-  fetchDataSurfSpots();
+fetchDataSurfSpots();
